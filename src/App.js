e4dd07c1735ef85c2704b2239ec75f3253479fc6
Megha Mathur <meghamathur347@gmail.com>
2023-06-22 16:41:00 +0530

    ihhi

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState }  from "react";
+import './index.css';
+const App = () => {
+  const state = useState();
+const [ count , setCount ]  = useState (0);
+
+const IncNum = () => {
+    setCount (count + 1);
+    console.log ("clicked"  + count+1);
+}
+
+
+    return (
+        <>
+            <h1> {count} </h1>
+             <button onClick = {IncNum}> Click Me </button>
+        </>
+    );
+    
+    };
+ export default App;
+
+
